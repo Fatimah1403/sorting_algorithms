6bf51f2f141e8a2f5c7802e07abf0ddcd4ad127a
fatimah1403 <fattyhassan14@gmail.com>
2023-04-25 21:54:08 +0000

    Task 2 update

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -24,24 +24,22 @@ void swap_ints(int *a, int *b)
  */
 void selection_sort(int *array, size_t size)
 {
+	int *min;
 	size_t i, j;
-	int *min_idx;
 
 	if (array == NULL || size < 2)
 		return;
+
 	for (i = 0; i < size - 1; i++)
 	{
-		min_idx = array + i;
+		min = array + i;
 		for (j = i + 1; j < size; j++)
-		{
-			min_idx = (array[j] < *min_idx) ? (array + j) : min_idx;
+			min = (array[j] < *min) ? (array + j) : min;
 
-		}
-		if ((array + i) != min_idx)
+		if ((array + i) != min)
 		{
-			swap_ints(array[i], min_idx);
+			swap_ints(array + i, min);
 			print_array(array, size);
 		}
 	}
-
 }
